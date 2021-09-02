5b33a0e20cc81e9ae1fc40acfa98476708fb823c
pasyagitka <lizavetazinovich@gmail.com>
2021-09-02 14:36:35 +0300

    init, add style.css

---
+++ JavaSpringI/src/main/java/meow/pasyagitka/javaspringi/JavaSpringIApplication.java
@@ -0,0 +1,13 @@
+package meow.pasyagitka.javaspringi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringIApplication.class, args);
+    }
+
+}
---
+++ JavaSpringI/src/test/java/meow/pasyagitka/javaspringi/JavaSpringIApplicationTests.java
@@ -0,0 +1,13 @@
+package meow.pasyagitka.javaspringi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
