f97a9d4983458862b9a23617053d4ae465caccae
pasyagitka <lizavetazinovich@gmail.com>
2021-09-27 18:58:42 +0300

    init

---
+++ TrainingVideos/src/main/java/meow/pasyagitka/trainingvideos/TrainingVideosApplication.java
@@ -0,0 +1,13 @@
+package meow.pasyagitka.trainingvideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingVideosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingVideosApplication.class, args);
+    }
+
+}
