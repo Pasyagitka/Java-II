4ac592997abd83afbc3dad4611b37305ad2248fe
pasyagitka <lizavetazinovich@gmail.com>
2021-10-27 21:03:10 +0300

    log aspect conf xml

--- FindTrainingVideos/src/main/java/meow/pasyagitka/findtrainingvideos/aspect/LogAspect.java
+++ FindTrainingVideos/src/main/java/meow/pasyagitka/findtrainingvideos/aspect/LogAspect.java
@@ -1,15 +1,10 @@
 package meow.pasyagitka.findtrainingvideos.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import org.apache.log4j.Logger;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 @Aspect
 @Component
@@ -17,17 +12,13 @@ public class LogAspect {
     Logger logger=Logger.getLogger(LogAspect.class);
 
     @Pointcut("execution(public * meow.pasyagitka.findtrainingvideos.controller.DisciplineController.*(..))")
-    public void callAtDisciplineController() {
-    }
+    public void callAtDisciplineController() {}
 
     @Before("callAtDisciplineController()")
-    public void beforeCallMethod(JoinPoint jp) {
-        String args = Arrays.stream(jp.getArgs()).map(a -> a.toString()).collect(Collectors.joining(","));
-        logger.info("before " + jp.toString() + ", args=[" + args + "]");
-    }
+    public void beforeCallMethod(JoinPoint jp) {logger.info("before " + jp.toString());}
+
     @After("callAtDisciplineController()")
     public void afterCallAt(JoinPoint jp) {
         logger.info("after " + jp.toString());
     }
-
 }
