4d48738c1e34644afdbf0a4d08e42b2fec93539d
pasyagitka <lizavetazinovich@gmail.com>
2021-11-29 10:07:22 +0300

    Open API doc AdminController

---
+++ FindTrainingVideos/src/main/java/meow/pasyagitka/findtrainingvideos/configuration/OpenAPIConfig.java
@@ -0,0 +1,30 @@
+package meow.pasyagitka.findtrainingvideos.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+   /* @Bean*/
+    /*public GroupedOpenApi publicUserApi() {
+        return GroupedOpenApi.builder()
+                .group("Users")
+                .pathsToMatch("/users/**")
+                .build();
+    }*/
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(new Info().title("Training Videos")
+                        .contact(new Contact().name("pasyagitka")
+                                .email("lizavetazinovich@gmail.com")))
+                .servers(List.of(new Server().url("http://localhost:8080")
+                                .description("Dev service")));
+    }
+}
--- FindTrainingVideos/src/main/java/meow/pasyagitka/findtrainingvideos/controller/AdminController.java
+++ FindTrainingVideos/src/main/java/meow/pasyagitka/findtrainingvideos/controller/AdminController.java
@@ -1,5 +1,11 @@
 package meow.pasyagitka.findtrainingvideos.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import meow.pasyagitka.findtrainingvideos.dto.VideoDto;
 import meow.pasyagitka.findtrainingvideos.exceptions.AddVideoException;
 import meow.pasyagitka.findtrainingvideos.exceptions.DeleteVideoException;
@@ -29,6 +35,10 @@ public class AdminController {
         return mav;
     }
 
+    @Operation(summary = "Gets list of all videos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Video list is present", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = VideoDto.class)) }),
+            @ApiResponse(responseCode = "500", description = "Error while returning video list", content = @Content)})
     @GetMapping(value = {"/adminmain"})
     public ResponseEntity<List<VideoDto>> getVideos() {
         try {
@@ -40,9 +50,15 @@ public class AdminController {
     }
 
     //todo dto for new video (no date,...)
+    @Operation(summary = "Adds a new video")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "New video is created", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = VideoDto.class)) }),
+            @ApiResponse(responseCode = "500", description = "Error while creating a new video", content = @Content)})
     @PostMapping("/adminmain/addvideo")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<VideoDto> saveVideo(@Valid @RequestBody VideoDto videoDto) throws AddVideoException {
+    public ResponseEntity<VideoDto> saveVideo(
+            @Parameter(description = "New video object")
+            @Valid @RequestBody VideoDto videoDto) throws AddVideoException {
         try {
             VideoDto v = videoService.save(videoDto);
             return new ResponseEntity<>(v, HttpStatus.CREATED);
@@ -51,8 +67,14 @@ public class AdminController {
         }
     }
 
+    @Operation(summary = "Updates a video by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The video is edited", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = VideoDto.class)) }),
+            @ApiResponse(responseCode = "404", description = "Video not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Error while editing a video", content = @Content)})
     @PutMapping("/adminmain/editvideo/{id}")
-    public ResponseEntity<VideoDto> updateVideo(@PathVariable("id") int id, @RequestBody VideoDto newVideoData) throws EditVideoException {
+    public ResponseEntity<VideoDto> updateVideo(@Parameter(description = "id of video to be edited")
+            @PathVariable("id") int id, @RequestBody VideoDto newVideoData) throws EditVideoException {
         try {
             VideoDto videoData = videoService.get(id);
 
@@ -73,8 +95,13 @@ public class AdminController {
         }
     }
 
+    @Operation(summary = "Deletes a video by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Video is deleted", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = VideoDto.class)) }),
+            @ApiResponse(responseCode = "500", description = "Error while deleting a video", content = @Content)})
     @DeleteMapping("/adminmain/deletevideo/{id}")
-    public ResponseEntity<HttpStatus> deleteVideo(@PathVariable("id") int id) throws DeleteVideoException {
+    public ResponseEntity<HttpStatus> deleteVideo(@Parameter(description = "id of video to be deleted")
+                                                      @PathVariable("id") int id) throws DeleteVideoException {
         try {
             videoService.delete(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
